a55c5187e478e59dcfcd8a59f270e2cddce49873
Adam <adamlundberg91@gmail.com>
2020-05-27 09:41:31 +0200

    Initial commit

---
+++ src/test/java/se/kalmar/adam/jpaworkshop/JpaWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package se.kalmar.adam.jpaworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
